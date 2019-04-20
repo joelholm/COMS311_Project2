5bbe4583cf5e9298d8594666569103c3d9fd8fae
Joel Holm <joeldholm@gmail.com>
2019-04-19 22:07:37 -0500

    Pretty much done with ComputerNode class. Not much more to do, other than extra error checking.

--- COMS311_Project2/src/ComputerNode.java
+++ COMS311_Project2/src/ComputerNode.java
@@ -1,17 +1,33 @@
 import java.util.*;
 
 public class ComputerNode {
-	public int getID() {
-		//TODO:
-		return -1;
-	}
-	public int getTimestamp() {
-		//TODO:
-		return -1;
-	}
-	public List<ComputerNode> getOutNeighbors(){
-		//TODO:
-		return null;
+	
+	private int timestamp;
+	private int id;
+	private List<ComputerNode> outNeighbors;
+	
+	public ComputerNode(int id, int timestamp) {
+		outNeighbors = new ArrayList<ComputerNode>();
+		this.timestamp = timestamp;
+		this.id = id;
 	}
 	
+	public int getID() {
+		return id;
+	}
+	
+	public int getTimestamp() {
+		return timestamp;
+	}
+	
+	public List<ComputerNode> getOutNeighbors(){
+		return outNeighbors;
+	}
+	
+	public void addNeighborNode(ComputerNode n) {
+		if( n == null ) {
+			throw new NullPointerException();
+		}
+		outNeighbors.add(n);
+	}
 }
--- COMS311_Project2/src/ComputerNodeTest.java
+++ COMS311_Project2/src/ComputerNodeTest.java
@@ -1,12 +1,39 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import java.util.*;
 
 class ComputerNodeTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+		ComputerNode[] n = new ComputerNode[6];
+		n[0] = new ComputerNode(1,1);
+		n[1] = new ComputerNode(2,2);
+		n[2] = new ComputerNode(3,3);
+		n[3] = new ComputerNode(1,4);
+		n[4] = new ComputerNode(2,6);
+		n[5] = new ComputerNode(4,9);
+		
+		n[0].addNeighborNode(n[1]);
+		n[0].addNeighborNode(n[3]);
+		n[1].addNeighborNode(n[3]);
+		n[1].addNeighborNode(n[4]);
+		n[2].addNeighborNode(n[5]);
+		n[3].addNeighborNode(n[5]);
+		n[4].addNeighborNode(n[5]);
+		
+		for( int j = 0; j < 6; j++) {
+			System.out.println("Node " + j + " neighbors");
+			List<ComputerNode> nebs = n[j].getOutNeighbors();
+			Iterator<ComputerNode> it = nebs.iterator();
+			int num = nebs.size();
+			for( int i = 0; i < num; i++ ) {
+				ComputerNode curr = it.next();
+				System.out.println("\tID: " + curr.getID() + " Timestamp: " + curr.getTimestamp());
+			}
+		}
 	}
 
 }
