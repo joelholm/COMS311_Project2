5fb1acd34bdb81db99bf5b4c4f75642e5f48f4d8
Colin Ishman <cdishman@iastate.edu>
2019-04-22 20:07:33 -0500

    Added work in progress BFS

--- COMS311_Project2/src/ComputerNode.java
+++ COMS311_Project2/src/ComputerNode.java
@@ -4,16 +4,36 @@ public class ComputerNode {
 	
 	private int timestamp;
 	private int id;
+	private int dist;
+	private ComputerNode pred;
 	private List<ComputerNode> outNeighbors;
 	
 	public ComputerNode(int id, int timestamp) {
 		outNeighbors = new ArrayList<ComputerNode>();
 		this.timestamp = timestamp;
 		this.id = id;
+		dist = 999;
+		pred = null;
 	}
 	
 	public int getID() {
 		return id;
+	}
+	
+	public int getDist() {
+		return dist;
+	}
+	
+	public void setDist(int d) {
+		dist = d;
+	}
+	
+	public ComputerNode getPred() {
+		return pred;
+	}
+	
+	public void setPred(ComputerNode ct) {
+		pred = ct;
 	}
 	
 	public int getTimestamp() {
